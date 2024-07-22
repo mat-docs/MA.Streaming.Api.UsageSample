18f9c670c13a4b995d635b89d30f36c3290f8a47
Owen Foo <owen.foo@mclarenapplied.com>
2024-07-22 10:38:18 +0100

    Add stream reader SQLRace example

---
+++ Python/stream_reader_sqlrace/SqlRace.py
@@ -0,0 +1,428 @@
+"""Pythonized version of common SQLRace calls"""
+
+import os
+from abc import ABC, abstractmethod
+import logging
+import socket
+
+# configure pythonnet runtime for SQLRace API
+os.environ["PYTHONNET_RUNTIME"] = "coreclr"
+os.environ["PYTHONNET_CORECLR_RUNTIME_CONFIG"] = (
+    r"C:\Program Files\McLaren Applied Technologies\ATLAS 10\MAT.Atlas.Host.runtimeconfig.json"
+)
+# only import clr after the runtime has been configured, so pylint: disable=wrong-import-position
+import clr
+
+
+logger = logging.getLogger(__name__)
+
+A10_INSTALL_PATH = r"C:\Program Files\McLaren Applied Technologies\ATLAS 10"
+SQL_RACE_DLL_PATH = rf"{A10_INSTALL_PATH}\MESL.SqlRace.Domain.dll"
+
+# Configure Pythonnet and reference the required assemblies for dotnet and SQL Race
+clr.AddReference("System.Collections")  # pylint: disable=no-member
+clr.AddReference("System.Core")  # pylint: disable=no-member
+clr.AddReference("System.IO")  # pylint: disable=no-member
+
+if not os.path.isfile(SQL_RACE_DLL_PATH):
+    raise FileNotFoundError(
+        "Couldn't find SQL Race DLL at "
+        + SQL_RACE_DLL_PATH
+        + " please check that Atlas 10 is installed"
+    )
+
+clr.AddReference(SQL_RACE_DLL_PATH)
+
+# .NET imports, so pylint: disable=wrong-import-position,wrong-import-order,import-error,wildcard-import
+from MAT.OCS.Core import (
+    SessionKey,
+)
+
+# .NET imports, so pylint: disable=wrong-import-position,wrong-import-order,import-error,wildcard-import
+from MESL.SqlRace.Domain import (
+    Core,
+    SessionManager,
+    SessionState,
+    RecordersConfiguration,
+)
+
+# .NET imports, so pylint: disable=wrong-import-position,wrong-import-order,import-error,wildcard-import
+from System import (
+    DateTime,
+    Guid,
+)
+
+# .NET imports, so pylint: disable=wrong-import-position,wrong-import-order,import-error,wildcard-import
+from System.Collections.Generic import (
+    List,
+)
+
+# .NET imports, so pylint: disable=wrong-import-position,wrong-import-order,import-error,wildcard-import
+from System.Net import (
+    IPEndPoint,
+    IPAddress,
+)
+
+
+def initialise_sqlrace():
+    """Check if SQLRace is initialised and initialise it if not."""
+    if not Core.IsInitialized:
+        logger.info("Initialising SQLRace API.")
+        Core.LicenceProgramName = "SQLRace"
+        Core.Initialize()
+        logger.info("SQLRace API initialised.")
+
+
+class SessionConnection(ABC):
+    """Abstract class that represents a session connection"""
+
+    initialise_sqlrace()
+    # .NET objects, so pylint: disable=invalid-name
+    sessionManager = SessionManager.CreateSessionManager()
+
+    @abstractmethod
+    def __init__(self):
+        self.client = None
+        raise NotImplementedError
+
+    @abstractmethod
+    def __enter__(self):
+        raise NotImplementedError
+
+    def close_session(self):
+        self.client.Session.EndData()
+        self.client.Close()
+        logger.info("Session closed.")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close_session()
+
+
+class SQLiteConnection(SessionConnection):
+    """Represents a connection to a ATLAS session in a SQLite database
+
+    This connections can either be reading from an existing session (mode = 'r') or
+    creating a new session (mode = 'w')
+
+    This Class supports the use of contex manager and will close the client session on
+    exit.
+
+    """
+
+    def __init__(
+        self,
+        db_location,
+        session_identifier: str = "",
+        session_key: str = None,
+        mode="r",
+        recorder=False,
+    ):
+        """Initializes a connection to a SQLite ATLAS session.
+
+        Args:
+            db_location: Location of SQLite database to connect to.
+            session_identifier: Name of the session identifier.
+            session_key: Session key of the session, leave it as None if creating a new
+                session
+            mode: read 'r' or  write 'w'.
+            recorder: Only applies in write mode, set to Ture to configure the SQLRace
+                Server Listener and  Recorder, so it can be viewed as a live session in
+                ATLAS.
+        """
+        self.client = None
+        self.session = None
+        self.db_location = db_location
+        self.session_identifier = session_identifier
+        self.mode = mode
+        self.recorder = recorder
+
+        if session_key is not None:
+            # .NET objects, so pylint: disable=invalid-name
+            self.sessionKey = SessionKey.Parse(session_key)
+        else:
+            self.sessionKey = None
+
+        if self.mode == "r":
+            self.load_session(session_key)
+        elif self.mode == "w":
+            self.create_sqlite()
+
+    @property
+    def connection_string(self):
+        return f"DbEngine=SQLite;Data Source={self.db_location};Pooling=false;"
+
+    def create_sqlite(self):
+        if self.recorder:
+            self.start_recorder()
+        self.sessionKey = SessionKey.NewKey()
+        sessionDate = DateTime.Now  # .NET objects, so pylint: disable=invalid-name
+        event_type = "Session"
+        logger.debug(
+            "Creating new session with connection string %s.", self.connection_string
+        )
+        # .NET objects, so pylint: disable=invalid-name
+        clientSession = self.sessionManager.CreateSession(
+            self.connection_string,
+            self.sessionKey,
+            self.session_identifier,
+            sessionDate,
+            event_type,
+        )
+        self.client = clientSession
+        self.session = clientSession.Session
+        logger.info("SQLite session created.")
+
+    def start_recorder(self, port=7300):
+        """Configures the SQL Server listener and recorder
+
+        Args:
+            port: Port number to open the Server Listener on.
+
+        """
+        # Find a port that is not in used
+        while is_port_in_use(port):
+            port += 1
+        logger.info("Opening server lister on port %d.", port)
+        # Configure server listener
+        Core.ConfigureServer(True, IPEndPoint(IPAddress.Parse("127.0.0.1"), port))
+        # .NET objects, so pylint: disable=invalid-name
+        recorderConfiguration = RecordersConfiguration.GetRecordersConfiguration()
+        recorderConfiguration.AddConfiguration(
+            Guid.NewGuid(),
+            "SQLite",
+            self.db_location,
+            self.db_location,
+            self.connection_string,
+            False,
+        )
+        if self.sessionManager.ServerListener.IsRunning:
+            logger.info(
+                "Server listener is running: %s.",
+                self.sessionManager.ServerListener.IsRunning,
+            )
+        else:
+            logger.warning(
+                "Server listener is running: %s.",
+                self.sessionManager.ServerListener.IsRunning,
+            )
+        logger.debug(
+            "Configuring recorder with connection string %s.", self.connection_string
+        )
+
+    def load_session(self, session_key: str = None):
+        """Loads a historic session from the SQLite database
+
+        Args:
+            session_key: Optional, updates the sessionKey attribute and opens that
+                session.
+
+        Returns:
+            None, session is opened and can be accessed from the attribute self.session.
+        """
+        if session_key is not None:
+            self.sessionKey = SessionKey.Parse(session_key)
+        elif self.sessionKey is None:
+            raise TypeError(
+                "load_session() missing 1 required positional argument: 'session_key'"
+            )
+        self.client = self.sessionManager.Load(self.sessionKey, self.connection_string)
+        self.session = self.client.Session
+
+        logger.info("SQLite session loaded.")
+
+    def __enter__(self):
+        return self.session
+
+
+class Ssn2Session(SessionConnection):
+    """Represents a session connection to a SSN2 file."""
+
+    def __init__(self, file_location):
+        self.sessionKey = None  # .NET objects, so pylint: disable=invalid-name
+        self.client = None
+        self.session = None
+        self.db_location = file_location
+
+    def load_session(self):
+        """Loads the session from the SSN2 file."""
+        connection_string = f"DbEngine=SQLite;Data Source= {self.db_location}"
+        # .NET objects, so pylint: disable=invalid-name
+        stateList = List[SessionState]()
+        stateList.Add(SessionState.Historical)
+
+        # Summary
+        summary = self.sessionManager.Find(connection_string, 1, stateList, False)
+        if summary.Count != 1:
+            logger.warning(
+                "SSN2 contains more than 1 session. Loading session %s. Consider "
+                "using 'SQLiteConnection' instead and specify the session key.",
+                summary.get_Item(0).Identifier,
+            )
+        self.sessionKey = summary.get_Item(0).Key
+        self.client = self.sessionManager.Load(self.sessionKey, connection_string)
+        self.session = self.client.Session
+
+        logger.info("SSN2 session loaded.")
+
+    def __enter__(self):
+        self.load_session()
+        return self.session
+
+
+class SQLRaceDBConnection(SessionConnection):
+    """Represents a connection to a ATLAS session in a SQLRace database
+
+    This connections can either be reading from an existing session (mode = 'r') or
+    creating a new session (mode = 'w')
+
+    This class supports the use of contex manager and will close the client session on
+    exit.
+
+    """
+
+    def __init__(
+        self,
+        data_source,
+        database,
+        session_identifier: str = "",
+        session_key: str = None,
+        mode="r",
+        recorder=False,
+    ):
+        """Initializes a connection to a SQLite ATLAS session.
+
+        Args:
+            data_source: Name or network address of the instance of SQL Server to
+                connect to.
+            database: Name of the database
+            session_identifier: Name of the session identifier.
+            session_key: Session key of the session, leave it as None if creating a new
+                session.
+            mode: read 'r' or  write 'w'.
+            recorder: Only applies in write mode, set to Ture to configure the  SQLRace
+                Server Listener and  Recorder, so it can be viewed as a live session in
+                ATLAS.
+        """
+        self.client = None
+        self.session = None
+        self.data_source = data_source
+        self.database = database
+        self.session_identifier = session_identifier
+        self.mode = mode
+        self.recorder = recorder
+
+        if session_key is not None:
+            # .NET objects, so pylint: disable=invalid-name
+            self.sessionKey = SessionKey.Parse(session_key)
+        else:
+            self.sessionKey = None
+
+        if self.mode == "r":
+            self.load_session(session_key)
+        elif self.mode == "w":
+            self.create_sqlrace()
+
+    @property
+    def connection_string(self):
+        return (
+            f"server={self.data_source};Initial Catalog={self.database};"
+            f"Trusted_Connection=True;"
+        )
+
+    def create_sqlrace(self):
+        if self.recorder:
+            self.start_recorder()
+        self.sessionKey = SessionKey.NewKey()
+        sessionDate = DateTime.Now  # .NET objects, so pylint: disable=invalid-name
+        event_type = "Session"
+        logger.debug(
+            "Creating new session with connection string %s.", self.connection_string
+        )
+        # .NET objects, so pylint: disable=invalid-name
+        clientSession = self.sessionManager.CreateSession(
+            self.connection_string,
+            self.sessionKey,
+            self.session_identifier,
+            sessionDate,
+            event_type,
+        )
+        self.client = clientSession
+        self.session = clientSession.Session
+        logger.info("SQLRace Database session created.")
+
+    def start_recorder(self, port=7300):
+        """Configures the SQL Server listener and recorder
+
+        Args:
+            port: Port number to open the Server Listener on.
+
+        """
+        # Find a port that is not in used
+        while is_port_in_use(port):
+            port += 1
+        logger.info("Opening server lister on port %d.", port)
+        # Configure server listener
+        Core.ConfigureServer(True, IPEndPoint(IPAddress.Parse("127.0.0.1"), port))
+
+        # Configure recorder
+        # .NET objects, so pylint: disable=invalid-name
+        recorderConfiguration = RecordersConfiguration.GetRecordersConfiguration()
+        recorderConfiguration.AddConfiguration(
+            Guid.NewGuid(),
+            "SQLServer",
+            rf"{self.data_source}\{self.database}",
+            rf"{self.data_source}\{self.database}",
+            self.connection_string,
+            False,
+        )
+        if self.sessionManager.ServerListener.IsRunning:
+            logger.info(
+                "Server listener is running: %s.",
+                self.sessionManager.ServerListener.IsRunning,
+            )
+        else:
+            logger.warning(
+                "Server listener is running: %s.",
+                self.sessionManager.ServerListener.IsRunning,
+            )
+        logger.debug(
+            "Configuring recorder with connection string %s.", self.connection_string
+        )
+
+    def load_session(self, session_key: str | None):
+        """Loads a historic session from the SQLRace database
+
+        Args:
+            session_key: Optional, updates the sessionKey attribute and opens that
+                session.
+
+        Returns:
+            session is opened and can be accessed from the attribute self.session.
+        """
+        if session_key is not None:
+            self.sessionKey = SessionKey.Parse(session_key)
+        elif self.sessionKey is None:
+            raise TypeError(
+                "load_session() missing 1 required positional argument: 'session_key'"
+            )
+        self.client = self.sessionManager.Load(self.sessionKey, self.connection_string)
+        self.session = self.client.Session
+
+        logger.info("SQLRace Database session loaded.")
+
+    def __enter__(self):
+        return self.session
+
+
+def is_port_in_use(port: int, ip="localhost") -> bool:
+    """Checks if the port is in use
+
+    Args:
+        port: port number
+        ip: ip
+
+    Returns:
+        True if the port is in use, else false.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex((ip, port)) == 0
---
+++ Python/stream_reader_sqlrace/main.py
@@ -0,0 +1,236 @@
+"""Read data packets from broker via the Stream API and write to a ATLAS session
+
+This example demonstrated how to consume data from the broker via the Stream API.
+The example will read packets for an ongoing session, and write the contents to an
+ATLAS Session.
+If there are no ongoing session, it will wait until a new one starts.
+
+See Also:
+    stream_writer_basic
+"""
+
+import asyncio
+import logging
+import signal
+
+import grpc
+import numpy as np
+
+from ma.streaming.api.v1 import api_pb2_grpc, api_pb2
+from ma.streaming.open_data.v1 import open_data_pb2
+from stream_api import StreamApi
+from atlas_session_writer import AtlasSessionWriter
+
+logger = logging.getLogger(__name__)
+
+
+class StreamReaderSql:
+    """Read data from the Stream API and write it to an ATLAS Session"""
+
+    def __init__(self, atlas_ssndb_location):
+        self.session_writer: AtlasSessionWriter
+        self.data_source = "SampleDataSource"
+        self.grpc_address = "localhost:13579"
+        self.stream_api = StreamApi(self.grpc_address)
+        self.session_key = None
+        self.main_task = None
+        self.atlas_ssndb_location = atlas_ssndb_location
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session_writer.close_session()
+        close_session_response = (
+            self.stream_api.connection_manager_service_stub.CloseConnection(
+                api_pb2.CloseConnectionRequest(connection=self.connection)
+            )
+        )
+        if close_session_response.success:
+            logger.info("Connection closed.")
+        else:
+            logger.warning("Connection was not successfully closed. ")
+
+    async def session_stop(self):
+        async with grpc.aio.insecure_channel(self.grpc_address) as channel:
+            session_management_stub = api_pb2_grpc.SessionManagementServiceStub(channel)
+            async for (
+                stop_notification
+            ) in session_management_stub.GetSessionStopNotification(
+                api_pb2.GetSessionStopNotificationRequest(data_source=self.data_source)
+            ):
+                print(stop_notification.session_key)
+                self.terminate_main_task()
+
+    def terminate_main_task(self, *_):
+        logger.info("Terminating main task.")
+        self.main_task.cancel()
+
+    async def read_essentials(self):
+        async with grpc.aio.insecure_channel(self.grpc_address) as channel:
+            packet_reader_stub = api_pb2_grpc.PacketReaderServiceStub(channel)
+            async for new_packet in packet_reader_stub.ReadEssentials(
+                api_pb2.ReadEssentialsRequest(connection=self.connection)
+            ):
+                logger.debug("New essential packet received.")
+                await self.handle_new_packet(new_packet)
+
+    async def read_packets(self):
+        async with grpc.aio.insecure_channel(self.grpc_address) as channel:
+            packet_reader_stub = api_pb2_grpc.PacketReaderServiceStub(channel)
+            async for new_packet in packet_reader_stub.ReadPackets(
+                api_pb2.ReadPacketsRequest(connection=self.connection)
+            ):
+                logger.debug("New packet received.")
+                await self.handle_new_packet(new_packet, True)
+
+    async def handle_new_packet(self, new_packet, match_session_key=False):
+
+        packet_type = new_packet.response[0].packet.type
+        content = new_packet.response[0].packet.content
+        session_key = new_packet.response[0].packet.session_key
+
+        # discard the packet if the session key does not match
+        if match_session_key and (session_key != self.session_key):
+            logger.info("Session key mismatch, packet discarded.")
+            return
+
+        # try and deserializes the packet, if the pack type can be located.
+        # if we can't find a corresponding packet type, discard the packet.
+        try:
+            packet_class = getattr(open_data_pb2, packet_type + "Packet")
+            packet = packet_class.FromString(content)
+        except AttributeError:
+            logger.debug(
+                "Unable to deserializes pack content for pack type %s", packet_type
+            )
+            return
+
+        if isinstance(packet, open_data_pb2.PeriodicDataPacket):
+            logger.debug("Data packet received.")
+            await self.handle_periodic_packet(packet)
+        elif isinstance(packet, open_data_pb2.ConfigurationPacket):
+            logger.debug("Config packet received.")
+            await self.handle_configuration_packet(packet)
+
+    async def handle_configuration_packet(
+        self, packet: open_data_pb2.ConfigurationPacket
+    ):
+        # Create a corresponding config in atlas
+        self.session_writer.add_configration(packet)
+
+    async def handle_periodic_packet(self, packet):
+        ##  Get the parameter identifier
+        if packet.data_format.data_format_identifier != 0:
+            data_format_manager_stub = self.stream_api.data_format_manager_service_stub
+            param_list_response = data_format_manager_stub.GetParametersList(
+                api_pb2.GetParametersListRequest(
+                    data_source=self.data_source,
+                    data_format_identifier=packet.data_format.data_format_identifier,
+                )
+            )
+            parameter_identifiers = param_list_response.parameters
+        else:
+            parameter_identifiers = (
+                packet.data_format.parameter_identifiers.parameter_identifiers
+            )
+
+        assert len(parameter_identifiers) == len(
+            packet.columns
+        ), "The number of parameter identifiers should match the number of columns"
+        ## Get the periodic data
+        start_time = packet.start_time
+        interval = packet.interval
+        data = []
+        status = []
+        for column, parameter_identifier in zip(packet.columns, parameter_identifiers):
+            samples = getattr(column, column.WhichOneof("list")).samples
+            data.append([s.value for s in samples])
+            status.append([s.status for s in samples])
+
+        ## Add the data to the session
+        # Create timestamps and convert them to SQLRace format.
+        timestamps_ns = [start_time + interval * i for i in range(len(data[0]))]
+        timestamps_sqlrace = np.mod(timestamps_ns, 1e9 * 3600 * 24)
+        # add the data to the session
+        for parameter_identifier, data_for_param in zip(parameter_identifiers, data):
+            self.session_writer.add_data(
+                parameter_identifier, data_for_param, timestamps_sqlrace
+            )
+
+    async def main(self):
+        # Create the gRPC clients
+        connection_management_stub = self.stream_api.connection_manager_service_stub
+        session_management_stub = self.stream_api.session_management_service_stub
+
+        # Get the latest live session
+        current_session_response = session_management_stub.GetCurrentSessions(
+            api_pb2.GetCurrentSessionsRequest(data_source=self.data_source)
+        )
+
+        for test_key in current_session_response.session_keys[::-1]:
+            session_info_response = session_management_stub.GetSessionInfo(
+                api_pb2.GetSessionInfoRequest(session_key=test_key)
+            )
+            if not session_info_response.is_complete:
+                self.session_key = test_key
+                logger.info("Identified live session %s", self.session_key)
+                break
+
+        # if there is no live session wait for a new one to start
+        if self.session_key is None:
+            logger.info("No live session found, waiting for new session to start.")
+            for new_session in session_management_stub.GetSessionStartNotification(
+                api_pb2.GetSessionStartNotificationRequest(data_source=self.data_source)
+            ):
+                self.session_key = new_session.session_key
+                logger.info("Identified live session %s", self.session_key)
+                break
+
+        session_info_response = session_management_stub.GetSessionInfo(
+            api_pb2.GetSessionInfoRequest(
+                session_key=self.session_key
+            )
+        )
+
+        # Establish a new connection
+        connection_details = api_pb2.ConnectionDetails(
+            data_source=self.data_source,
+            session=self.session_key,
+            main_offset=session_info_response.main_offset,
+            essentials_offset=session_info_response.essentials_offset,
+        )
+
+        connection_response = connection_management_stub.NewConnection(
+            api_pb2.NewConnectionRequest(details=connection_details)
+        )
+
+        self.connection = connection_response.connection
+
+        # Create a corresponding ATLAS session
+        self.session_writer = AtlasSessionWriter(self.atlas_ssndb_location)
+
+        # Read essential stream which contains essential information such as configs
+        await self.read_essentials()
+
+        # Set up a handler if we want to terminate early by ctrl+c
+        signal.signal(signal.SIGINT, self.terminate_main_task)
+
+        # Read packets and monitor session stop at the same time
+        self.main_task = asyncio.gather(
+            self.session_stop(),
+            self.read_packets(),
+        )
+        try:
+            logger.debug("Starting main task.")
+            await self.main_task
+        except asyncio.CancelledError:
+            logger.info("Terminating...")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    db_location = r"C:\McLaren Applied\StreamAPIDemo.ssndb"
+    with StreamReaderSql(db_location) as stream_recorder:
+        asyncio.run(stream_recorder.main())
