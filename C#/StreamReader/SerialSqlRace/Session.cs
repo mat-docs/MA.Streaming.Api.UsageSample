4a0ba2bdc3d5e73bfc85cb0fd6e20a276b8904ba
Salman Alfarisyi <salman.alfarisyi@mclarenapplied.com>
2024-08-21 15:07:16 +0100

    fixed resource exhaustion issue

--- C#/StreamReader/SerialSqlRace/Session.cs
+++ C#/StreamReader/SerialSqlRace/Session.cs
@@ -83,9 +83,6 @@ namespace Stream.Api.Stream.Reader.SerialSqlRace
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Failed to read stream due to {ex}");
-                }
-                finally
-                {
                     ReadPackets(cancellationToken, connectionDetails);
                 }
             }, cancellationToken);
