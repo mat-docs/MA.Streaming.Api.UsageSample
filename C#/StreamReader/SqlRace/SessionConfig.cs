31e031a108be23ca691e8956ff5156d818b43aa1
Salman Alfarisyi <salman.alfarisyi@mclarenapplied.com>
2024-09-04 17:35:36 +0100

    Simiplified the config initialization

--- C#/StreamReader/SqlRace/SessionConfig.cs
+++ C#/StreamReader/SqlRace/SessionConfig.cs
@@ -5,16 +5,9 @@ namespace Stream.Api.Stream.Reader.SqlRace
 {
     internal class SessionConfig
     {
-        public ConcurrentDictionary<string, uint> ChannelIdRowParameterDictionary { get; set; } 
-        public ConcurrentDictionary<string, Dictionary<uint, uint>> ChannelIdPeriodicParameterDictionary { get; set; }
-        public ConcurrentDictionary<string, EventDefinition> EventDefCache { get; set; }
-
-        public SessionConfig()
-        {
-            ChannelIdPeriodicParameterDictionary = new ConcurrentDictionary<string, Dictionary<uint, uint>>();
-            ChannelIdRowParameterDictionary = new ConcurrentDictionary<string, uint>();
-            EventDefCache = new ConcurrentDictionary<string, EventDefinition>();
-        }
+        public ConcurrentDictionary<string, uint> ChannelIdRowParameterDictionary { get; } = new();
+        public ConcurrentDictionary<string, Dictionary<uint, uint>> ChannelIdPeriodicParameterDictionary { get; } = new();
+        public ConcurrentDictionary<string, EventDefinition> EventDefCache { get; } = new();
 
         public void Dispose()
         {
