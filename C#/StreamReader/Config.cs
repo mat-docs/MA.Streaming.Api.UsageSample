767f4a7b6990cc48f3d035b34238e506f8f8f2f9
Salman Alfarisyi <salman.alfarisyi@mclarenapplied.com>
2024-09-24 11:50:10 +0100

    fixed config.cs to actually accept configs.

--- C#/StreamReader/Config.cs
+++ C#/StreamReader/Config.cs
@@ -5,10 +5,10 @@ namespace Stream.Api.Stream.Reader
 {
     public class Config
     {
-        public string DataSource { get; } = "Default";
+        public string DataSource { get; set; } = "Default";
 
-        public string SQLRaceConnectionString { get; } = "server=MCLA-F8ZLSQ3\\LOCAL;Initial Catalog=SQLRACE01_LOCAL;Trusted_Connection=True;";
+        public string SQLRaceConnectionString { get; set; } = "server=MCLA-F8ZLSQ3\\LOCAL;Initial Catalog=SQLRACE01_LOCAL;Trusted_Connection=True;";
 
-        public string IPAddress { get; } = "127.0.0.1:13579";
+        public string IPAddress { get; set; } = "127.0.0.1:13579";
     }
 }
